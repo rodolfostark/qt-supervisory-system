90f006753535f4e867c9c6930a4a8a935135a112
rodolfostark <rodolfostark@ufrn.edu.br>
2018-11-28 00:52:30 -0300

    Concluindo a primeira parte do systema supervisório com qt.

--- QtTcpClientProducer/mainwindow.cpp
+++ QtTcpClientProducer/mainwindow.cpp
@@ -16,44 +16,87 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->horizontalMinSlider,
             SIGNAL(valueChanged(int)),
             this, SLOT(minLcd()));
+
     connect(ui->horizontalMaxSlider,
             SIGNAL(valueChanged(int)),
             this,
             SLOT(maxLcd()));
+
     connect(ui->timingSlider,
             SIGNAL(valueChanged(int)),
             this,
             SLOT(timingLabel()));
+
     connect(ui->ConnectButton,
             SIGNAL(clicked(bool)),
             this,
             SLOT(conectar()));
+
     connect(ui->DisconnectButton,
-           SIGNAL(clicked(bool)),
-           this,
-           SLOT(desconectar()));
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(desconectar()));
+
+    connect(ui->horizontalMaxSlider,
+            SIGNAL(valueChanged(int)),
+            this,
+            SLOT(setValorMax()));
+
+    connect(ui->horizontalMinSlider,
+            SIGNAL(valueChanged(int)),
+            this,
+            SLOT(setValorMin()));
+
+    connect(ui->StartButton,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(startTransmit()));
+
+    connect(ui->StopButton,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(stopTransmit()));
 }
 
-void MainWindow::tcpConnect(){
+void MainWindow::tcpConnect()
+{
     socket->connectToHost("127.0.0.1",1234);
     if(socket->waitForConnected(3000)){
-        qDebug() << "Connected";
+        qDebug() << "Connected!";
     }
     else{
-        qDebug() << "Disconnected";
+        qDebug() << "Disconnected!";
     }
 }
 
-void MainWindow::putData(){
+void MainWindow::timerEvent(QTimerEvent *event)
+{
+    QDateTime datetime;
+    QString str;
+    qint64 msecdate;
+    if(socket->state()== QAbstractSocket::ConnectedState){
+        msecdate = QDateTime::currentDateTime().toMSecsSinceEpoch();
+        str = "set "+ QString::number(msecdate) + " " + QString::number((qrand()%(valorMax - valorMin +1)) + valorMin)+"\r\n";
+        mensagem = str;
+        qDebug() << str;
+        qDebug() << socket->write(str.toStdString().c_str()) << " bytes written";
+        if(socket->waitForBytesWritten(3000)){
+            qDebug() << "wrote";
+        }
+    }
+    ui->textBrowser->append(mensagem);
+}
+
+void MainWindow::putData()
+{
     QDateTime datetime;
     QString str;
     qint64 msecdate;
 
     if(socket->state()== QAbstractSocket::ConnectedState){
-
         msecdate = QDateTime::currentDateTime().toMSecsSinceEpoch();
-        str = "set "+ QString::number(msecdate) + " " + QString::number(qrand()%35)+"\r\n";
-
+        str = "set "+ QString::number(msecdate) + " " + QString::number((qrand()%(valorMax - valorMin +1)) + valorMin)+"\r\n";
+        mensagem = str;
         qDebug() << str;
         qDebug() << socket->write(str.toStdString().c_str()) << " bytes written";
         if(socket->waitForBytesWritten(3000)){
@@ -74,6 +117,11 @@ void MainWindow::maxLcd()
 
 void MainWindow::timingLabel()
 {
+    time = ui->timingSlider->value()*1000;
+    if(useTimer){
+        killTimer(timer);
+        timer = startTimer(time);
+    }
     QString valor = QString::number(ui->timingSlider->value());
     ui->timingLabelValue->setText(valor);
 }
@@ -83,10 +131,10 @@ void MainWindow::conectar()
     QString host = ui->ipLineEdit->text();
     socket->connectToHost(host, 1234);
     if(socket->waitForConnected(3000)){
-        qDebug() << "Connected";
+        qDebug() << "Connected!";
     }
     else{
-        qDebug() << "Disconnected";
+        qDebug() << "Disconnected!";
     }
 
 }
@@ -100,6 +148,33 @@ void MainWindow::desconectar()
     }
 }
 
+void MainWindow::setValorMax()
+{
+    valorMax = ui->horizontalMaxSlider->value();
+    qDebug() << "Valor máximo: " << valorMax;
+}
+
+void MainWindow::setValorMin()
+{
+    valorMin = ui->horizontalMinSlider->value();
+    qDebug() << "Valor mínimo: " << valorMin;
+}
+
+void MainWindow::startTransmit()
+{
+    if(useTimer == false){
+        timer = startTimer(time);
+        useTimer = true;
+    }
+}
+
+void MainWindow::stopTransmit()
+{
+    if(timer == true){
+        killTimer(timer);
+        useTimer = false;
+    }
+}
 
 MainWindow::~MainWindow(){
     delete socket;
--- QtTcpClientProducer/mainwindow.h
+++ QtTcpClientProducer/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QTcpSocket>
 #include <QDebug>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -11,23 +12,35 @@ class MainWindow;
 
 class MainWindow : public QMainWindow
 {
-  Q_OBJECT
+    Q_OBJECT
 
 public:
-  explicit MainWindow(QWidget *parent = 0);
-  ~MainWindow();
-  void tcpConnect();
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void tcpConnect();
+    void timerEvent(QTimerEvent *event);
+
 public slots:
-  void putData();
-  void minLcd();
-  void maxLcd();
-  void timingLabel();
-  void conectar();
-  void desconectar();
+    void putData();
+    void minLcd();
+    void maxLcd();
+    void timingLabel();
+    void conectar();
+    void desconectar();
+    void setValorMax();
+    void setValorMin();
+    void startTransmit();
+    void stopTransmit();
 
 private:
-  Ui::MainWindow *ui;
-  QTcpSocket *socket;
+    int valorMax = 1;
+    int valorMin = 1;
+    int time = 800;
+    int timer;
+    QString mensagem = "Não conectado ao servidor.";
+    bool useTimer = false;
+    Ui::MainWindow *ui;
+    QTcpSocket *socket;
 };
 
 #endif // MAINWINDOW_H
