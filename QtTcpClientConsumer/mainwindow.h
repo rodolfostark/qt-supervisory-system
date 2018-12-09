28890d7217ed0b83cad1cbca99dcb8cd8e105ba0
rodolfostark <rodolfostark@ufrn.edu.br>
2018-12-08 22:01:49 -0300

    Iniciando a construção do módulo consumidor de dados.

--- QtTcpClientConsumer/mainwindow.h
+++ QtTcpClientConsumer/mainwindow.h
@@ -16,11 +16,20 @@ class MainWindow : public QMainWindow
 public:
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
-  
   void tcpConnect();
+  void timerEvent(QTimerEvent *event);
 public slots:
   void getData();
+  void conectar();
+  void desconectar();
+  void setTimingValue();
+  void getListOfHosts();
+  void startReceive();
+  void stopReceive();
 private:
+  int time = 1;
+  int timer;
+  bool useTimer = false;
   Ui::MainWindow *ui;
   QTcpSocket *socket;
 };
