45d03c9b7cd4f36b54f9cf775367e8f597e5a841
rodolfostark <rodolfostark@ufrn.edu.br>
2018-12-11 11:25:44 -0300

    Recebendo dados do servidor e enviando para o objeto grafico para plotar na tela.

---
+++ QtTcpClientConsumer/grafico.cpp
@@ -0,0 +1,6 @@
+#include "grafico.h"
+
+Grafico::Grafico(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ QtTcpClientConsumer/grafico.h
@@ -0,0 +1,17 @@
+#ifndef GRAFICO_H
+#define GRAFICO_H
+
+#include <QWidget>
+
+class Grafico : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Grafico(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // GRAFICO_H
